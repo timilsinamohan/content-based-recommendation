9010b7508a31be11921e736d93d27a8c420ac5a5
Mohan <mohtim@dsiadmins-MBP-3.lan>
2025-02-14 16:10:19 -0500

    Fix Google Drive file download in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,17 @@ pip install -r requirements.txt
 
 # Download movies_dict.pkl from Google Drive
 echo "Downloading movies_dict.pkl..."
-wget -O movies_dict.pkl "https://drive.google.com/uc?export=download&id=1lGGJ6rqaKU1lCzTyR7joWL1YosvgeNna"
+FILE_ID="1lGGJ6rqaKU1lCzTyR7joWL1YosvgeNna"
+FILE_NAME="movies_dict.pkl"
+curl -L -o ${FILE_NAME} "https://drive.google.com/uc?export=download&id=${FILE_ID}"
 
 # Download similarity.pkl from Google Drive
 echo "Downloading similarity.pkl..."
-wget -O similarity.pkl "https://drive.google.com/uc?export=download&id=1Jfy8zB_MwJg343HbtvD4tPutADg4Ak_F"
+FILE_ID="1Jfy8zB_MwJg343HbtvD4tPutADg4Ak_F"
+FILE_NAME="similarity.pkl"
+curl -L -c cookies.txt "https://drive.google.com/uc?export=download&id=${FILE_ID}" | grep -o 'confirm=[^&]*' | cut -d '=' -f 2 > confirm.txt
+CONFIRM=$(cat confirm.txt)
+curl -L -b cookies.txt -o ${FILE_NAME} "https://drive.google.com/uc?export=download&id=${FILE_ID}&confirm=${CONFIRM}"
+rm cookies.txt confirm.txt
 
 echo "Setup complete!"
